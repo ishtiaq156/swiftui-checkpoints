e9db2a450f51fcc02fb830b22371c65c53b511d4
Ishtiaq Ul Haq Syed <ishtiaq156@gmail.com>
2023-10-20 20:03:24 -0400

    add checkpoints 1 and 2

---
+++ checkpoint-1.playground/Contents.swift
@@ -0,0 +1,6 @@
+import Cocoa
+
+let celsiusTemp = 37.0
+let fahrenheitTemp = ((celsiusTemp * 9) / 5) + 32
+
+print("Celsius Temperature \(celsiusTemp) in Fahrenheit is \(fahrenheitTemp)")
---
+++ checkpoint-2.playground/Contents.swift
@@ -0,0 +1,8 @@
+import Cocoa
+
+var numbers: [String] = ["One", "Two", "Three", "One"]
+
+print(numbers.count)
+
+
+print((Set(numbers)).count)
