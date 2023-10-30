68eb7c571d3d67f1f773583d4023a57278b5abcd
Ishtiaq Ul Haq Syed <ishtiaq156@gmail.com>
2023-10-29 23:32:54 -0400

    formatting

--- checkpoint-5.playground/Contents.swift
+++ checkpoint-5.playground/Contents.swift
@@ -4,9 +4,9 @@ let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
 
 let luckyNumbersFormatted = { (luckyNumbersArray: [Int]) in
     luckyNumbersArray
-        .filter {!$0.isMultiple(of: 2)}
+        .filter { !$0.isMultiple(of: 2) }
         .sorted()
-        .map({print("\($0) is a lucky number")})
+        .map { print("\($0) is a lucky number") }
 }
 
 luckyNumbersFormatted(luckyNumbers)
